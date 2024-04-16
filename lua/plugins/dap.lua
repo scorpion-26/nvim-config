db36d4feec869a281b656ff55dd401586b2f1cf0
scorpion-26 <dev.scorpion26@gmail.com>
2024-04-16 15:48:57 +0200

    Change breakpoint icon

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -14,3 +14,7 @@ dap.configurations.lua = {
 dap.adapters.nlua = function(callback, config)
     callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
 end
+
+-- Setup icons
+vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#FFFFFF"})
+vim.fn.sign_define('DapBreakpoint', {text='', texthl='DapBreakpoint'})
